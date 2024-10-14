3c3a964f9db0a312dc885841efda138dbb8ce204
Madani00 <mamchart@gmail.com>
2024-10-14 23:55:35 +0100

    well done with the front design

--- 0-java.js
+++ 0-java.js
@@ -1,10 +1,30 @@
 
 const searching = document.querySelector(".searching")
 
-//  take the input and store is in userMessage
+const chat_list = document.querySelector(".chat_list")
 
-searching.addEventListener("submit" , (s) => {
-    s.preventDefault();
-    const userMessage = document.querySelector(".inputtt").value;
-    console.log(userMessage);
+searching.addEventListener("submit" , (event) => {
+    event.preventDefault();
+
+    const userMsg = document.querySelector(".inputtt").value;
+    // console.log(userMessage);
+
+    // if there is no input
+    if (!userMsg) return;
+
+    // if there is an input
+    const html = `
+                    <div class="message_list">
+                        <img src="imgs/user.jpg" alt="">
+                        <p class="text"></p>
+                    </div>
+    `
+
+    const div = document.createElement("div");   // creates a new <div> element in the DOM.
+    div.classList.add("message", "outgoing");     // adds two classes, "message" and "outgoing", to the newly created <div>
+    div.innerHTML =  html       // Setting the inner HTML to the previos html variable
+    div.querySelector(".text").innerHTML = userMsg    // Replacing the message text to useMsg
+
+    chat_list.appendChild(div)
+    searching.reset()     // to erase the message from searching
 })
